e659e28b1902b858c16c6327f78fe8035b22332c
Ben Evans <ben@bensbit.co.uk>
2012-08-16 15:03:53 +0100

    time_ago Template Helper

--- server.js
+++ server.js
@@ -161,6 +161,40 @@ hbs.registerHelper('gravatar_url', function(gravatarHash, size) {
 	return "http://www.gravatar.com/avatar/" + gravatarHash + "?s=" + size || 140;
 });
 
+hbs.registerHelper('time_ago', function(date, size) {
+
+	// Inspired by https://github.com/elving/swag/blob/00b3213de4811c0f27cb46c89b8ddae6b2e15702/src/swag.dates.coffee#L94
+
+	date = new Date(date)
+    seconds = Math.floor((new Date() - date) / 1000)
+
+    interval = Math.floor(seconds / 31536000)
+    if (interval > 1)
+    return interval + " years ago"
+
+    interval = Math.floor(seconds / 2592000)
+    if (interval > 1)
+    return interval + " months ago"
+
+    interval = Math.floor(seconds / 86400)
+    if (interval > 1)
+    return interval + " days ago"
+
+    interval = Math.floor(seconds / 3600)
+    if (interval > 1)
+    return interval + " hours ago"
+
+    interval = Math.floor(seconds / 60)
+    if (interval > 1)
+    return interval + " minutes ago"
+
+    if (Math.floor(seconds) == 0)
+    	return 'Just now'
+    else
+    	return Math.floor(seconds) + ' seconds ago';
+
+});
+
 
 require('./app/controllers');
 
