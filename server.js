dd906e3240a0498fe7fc62095aebebd84095f530
Ben Evans <ben@bensbit.co.uk>
2012-08-19 22:20:25 +0100

    Default Project Widget Image

--- server.js
+++ server.js
@@ -209,7 +209,7 @@ hbs.registerHelper('time_ago', function(date, size) {
 
 hbs.registerHelper('project_widget', function(project) {
 
-	projectImage = '/';
+	projectImage = '/assets/img/DefaultProjectWidget.png';
 	return [' <div class="projectwidget" onclick="location.href=\'/project/' + project.name + '\';" style="background-image:url(\'' + projectImage + '\');">',
 				'<div class="info">',
 				'	<h3>' + project.name + '</h3><p>Created By: <a href="user/' + project.creator + '">' + '</a>.</p>',
