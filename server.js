2a5340996d8a63a015298861fd17b86c8eefd7cd
Ben Evans <ben@bensbit.co.uk>
2012-08-19 18:02:12 +0100

    Project Widget View Helper

--- server.js
+++ server.js
@@ -205,6 +205,17 @@ hbs.registerHelper('time_ago', function(date, size) {
 
 });
 
+hbs.registerHelper('project_widget', function(project) {
+
+	projectImage = '/';
+	return [' <div class="projectwidget" onclick="location.href=\'/project/' + project.name + '\';" style="background-image:url(\'' + projectImage + '\');">',
+				'<div class="info">',
+				'	<h3>' + project.name + '</h3><p>Created By: <a href="user/' + project.creator + '">' + '</a>.</p>',
+				'</div>',
+			'</div>'].join('');
+
+});
+
 app.get('/500', function(req, res, next) {
 	next('Test Error');
 });
