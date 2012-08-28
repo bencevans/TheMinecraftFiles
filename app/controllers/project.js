44c15d220b5d76af3b1274f77646351a2d3de35e
Ben Evans <ben@bensbit.co.uk>
2012-08-28 20:18:35 +0100

    Started Import of Project SubPages

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -62,17 +62,49 @@ app.post('/new', function (req, res, next) {
 });
 
 app.get('/project/:projectSlug', function (req, res, next) {
+	res.redirect('/project/' + req.params.projectSlug + '/timeline');
+});
+
+app.all('/project/:projectSlug/:subPage', function (req, res, next) {
 
 	tmf.getProject(req.params.projectSlug, function (err, project) {
 
 		if(err) return next(err);
 
 		if(!project) return res.render('errors/404', {status:404});
-		res.locals.title = project.name;
 
-		res.render('project', {project:project});
+		res.locals.title = project.name;
+		req.project = res.locals.project = project;
+		res.locals.subPages = [{name:'Timeline', slug:'timeline', url:'/project/' + project.name + '/timeline'},
+		{name:'Gallery', slug:'gallery', url:'/project/' + project.name + '/gallery'},
+		{name:'Downloads', slug:'downloads', url:'/project/' + project.name + '/downloads'},
+		{name:'Settings', slug:'settings', url:'/project/' + project.name + '/settings'},
+		{name:'Issues', slug:'issues', url:'/project/' + project.name + '/issues'}]
+
+		next();
 
 	});
 
 });
 
+
+app.get('/project/:projectSlug/timeline', function (req, res, next) {
+
+	res.render('project/timeline', {layout:false}, function (err, html) {
+		if(err) return next(err);
+		res.render('project', {subPage:{content:html, name:'Timeline', slug:'timeline'}})
+	});
+
+});
+
+app.get('/project/:projectSlug/downloads', function (req, res, next) {
+	next(404)
+});
+
+app.get('/project/:projectSlug/gallery', function (req, res, next) {
+	next(404)
+});
+
+app.get('/project/:projectSlug/settings', function (req, res, next) {
+	next(404)
+});
