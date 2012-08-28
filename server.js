f3b7840972faf2b90024a000441464d7ca2b2c03
Ben Evans <ben@bensbit.co.uk>
2012-08-28 19:14:13 +0100

    getUser resolves/populates child projects

--- server.js
+++ server.js
@@ -263,7 +263,7 @@ hbs.registerHelper('project_widget', function(project) {
 	projectImage = '/assets/img/DefaultProjectWidget.png';
 	return [' <div class="projectwidget" onclick="location.href=\'/project/' + project.name + '\';" style="background-image:url(\'' + projectImage + '\');">',
 				'<div class="info">',
-				'	<h3>' + project.name + '</h3><p>Created By: <a href="user/' + project.creator + '">' + '</a>.</p>',
+				'	<h3>' + project.name + '</h3><p>Created By: <a href="user/' + project.creator.username + '">' + project.creator.username + '</a>.</p>',
 				'</div>',
 			'</div>'].join('');
 
