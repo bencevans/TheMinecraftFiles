3024a48df2723ce3c75b661559d5a806cf6b1778
Ben Evans <ben@bensbit.co.uk>
2013-01-10 09:28:31 +0000

    Return 404 if No Project in API

--- api/routes/project.js
+++ api/routes/project.js
@@ -1,12 +1,14 @@
 module.exports = function (req, res, next) {
   tmf.getProject(req.params.project, function (err, project) {
     if(err) return next(err);
+    if(!project) {
+      res.status(404);
+      return res.send({message:'Project Not Found'});
+    }
     project.getImage(function (err) {
       project.getCreator(function (err) {
         if(err) return next(err);
-        if(project) return res.send(project);
-        res.status(404);
-        res.send({message:'Project Not Found'});
+        return res.send(project);
       });
     });
   });
